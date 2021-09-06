69689b242eeafdcc63273bd21bb09a4204517e5e
Dominik Fuchß <dominik.fuchss@kit.edu>
2021-09-06 15:42:25 +0200

    Just some bugfix regarding missing fields in Artemis responses

--- bundles/edu.kit.kastel.sdq.eclipse.grading.client/src/edu/kit/kastel/sdq/eclipse/grading/client/mappings/ArtemisExercise.java
+++ bundles/edu.kit.kastel.sdq.eclipse.grading.client/src/edu/kit/kastel/sdq/eclipse/grading/client/mappings/ArtemisExercise.java
@@ -13,83 +13,87 @@ import edu.kit.kastel.sdq.eclipse.grading.client.rest.ArtemisRESTClient;
 
 public class ArtemisExercise implements IExercise {
 
-	@JsonProperty(value = "id")
-	private int exerciseId;
-	@JsonProperty
-	private String title;
-	@JsonProperty
-	private String shortName;
-	@JsonProperty
-	private String testRepositoryUrl;
-	@JsonProperty
-	private Boolean secondCorrectionEnabled;
-	private transient Collection<ISubmission> submissions;
+    @JsonProperty(value = "id")
+    private int exerciseId;
+    @JsonProperty
+    private String title;
+    @JsonProperty
+    private String shortName;
+    @JsonProperty
+    private String testRepositoryUrl;
+    @JsonProperty
+    private Boolean secondCorrectionEnabled;
+    private transient Collection<ISubmission> submissions;
 
 
-	/**
+    /**
 	 * For Auto-Deserialization
 	 * Need to call this::init thereafter!
-	 */
-	public ArtemisExercise() {
+     */
+    public ArtemisExercise() {
 
-	}
+    }
 
-	public ArtemisExercise(int exerciseId, String title, String shortName, String testRepositoryUrl, Collection<ISubmission> submissions) {
-		this.exerciseId = exerciseId;
-		this.title = title;
-		this.shortName = shortName;
-		this.testRepositoryUrl = testRepositoryUrl;
-		this.submissions = submissions;
-	}
+    public ArtemisExercise(int exerciseId, String title, String shortName, String testRepositoryUrl, Collection<ISubmission> submissions) {
+        this.exerciseId = exerciseId;
+        this.title = title;
+        this.shortName = shortName;
+        this.testRepositoryUrl = testRepositoryUrl;
+        this.submissions = submissions;
+    }
 
-	@Override
-	public int getExerciseId() {
-		return this.exerciseId;
-	}
+    @Override
+    public int getExerciseId() {
+        return this.exerciseId;
+    }
 
-	@Override
-	public Boolean getSecondCorrectionEnabled() {
-		return this.secondCorrectionEnabled;
-	}
+    @Override
+    public Boolean getSecondCorrectionEnabled() {
+        return this.secondCorrectionEnabled;
+    }
 
-	@Override
-	public String getShortName() {
-		return this.shortName;
-	}
+    @Override
+    public String getShortName() {
+		// TODO Just a bugfix . Filter non programming exercises by using the type field !!
+        if (shortName == null) {
+            return this.title;
+        }
+        return this.shortName;
+    }
 
-	@Override
-	public Collection<ISubmission> getSubmissions() {
-		return this.submissions;
-	}
+    @Override
+    public Collection<ISubmission> getSubmissions() {
+        return this.submissions;
+    }
 
-	@Override
-	public String getTestRepositoryUrl() {
-		return this.testRepositoryUrl;
-	}
+    @Override
+    public String getTestRepositoryUrl() {
+        return this.testRepositoryUrl;
+    }
 
-	@Override
-	public String getTitle() {
-		return this.title;
-	}
+    @Override
+    public String getTitle() {
+        return this.title;
+    }
 
-	public void init(ArtemisRESTClient artemisRESTClient) throws AuthenticationException, JsonProcessingException {
-		this.submissions = artemisRESTClient.getSubmissionsForExercise(this);
-	}
+    public void init(ArtemisRESTClient artemisRESTClient) throws AuthenticationException, JsonProcessingException {
+        this.submissions = artemisRESTClient.getSubmissionsForExercise(this);
+    }
 
-	/**
-	 *
-	 * @return a String like {@code toString}, but with fields not contained in IExercise
-	 */
-	public String toDebugString() {
+    /**
+     *
+     * @return a String like {@code toString}, but with fields not contained in IExercise
+     */
+    public String toDebugString() {
 		return "ArtemisExercise [exerciseId=" + this.exerciseId + ", title=" + this.title + ", shortName=" + this.shortName
 				+ ", testRepositoryUrl=" + this.testRepositoryUrl + ", submissions=" + this.submissions + "]";
-	}
+    }
 
-	@Override
-	public String toString() {
+    @Override
+    public String toString() {
 		return "ArtemisExercise [exerciseId=" + this.exerciseId + ", title=" + this.title + ", shortName=" + this.shortName
 				+ ", submissions=" + this.submissions + "]";
-	}
+    }
 
 
 
--- bundles/edu.kit.kastel.sdq.eclipse.grading.client/src/edu/kit/kastel/sdq/eclipse/grading/client/mappings/exam/ArtemisExerciseGroup.java
+++ bundles/edu.kit.kastel.sdq.eclipse.grading.client/src/edu/kit/kastel/sdq/eclipse/grading/client/mappings/exam/ArtemisExerciseGroup.java
@@ -1,6 +1,7 @@
 package edu.kit.kastel.sdq.eclipse.grading.client.mappings.exam;
 
 import java.util.Collection;
+import java.util.List;
 import java.util.stream.Collectors;
 
 import javax.security.sasl.AuthenticationException;
@@ -15,53 +16,56 @@ import edu.kit.kastel.sdq.eclipse.grading.client.rest.ArtemisRESTClient;
 
 public class ArtemisExerciseGroup implements IExerciseGroup {
 
-	@JsonProperty(value = "id")
-	private int exerciseGroupId;
-	@JsonProperty
-	private String title;
-	@JsonProperty
-	private boolean isMandatory;
-	@JsonProperty
-	private Collection<ArtemisExercise> exercises;
+    @JsonProperty(value = "id")
+    private int exerciseGroupId;
+    @JsonProperty
+    private String title;
+    @JsonProperty
+    private boolean isMandatory;
+    @JsonProperty
+    private Collection<ArtemisExercise> exercises;
 
-	/**
+    /**
 	 * For Auto-Deserialization
 	 * Need to call this::init thereafter!
-	 */
+     */
 	public ArtemisExerciseGroup() {	}
 
-	@Override
-	public int getExerciseGroupId() {
-		return this.exerciseGroupId;
-	}
+    @Override
+    public int getExerciseGroupId() {
+        return this.exerciseGroupId;
+    }
 
-	@Override
-	public Collection<IExercise> getExercises() {
+    @Override
+    public Collection<IExercise> getExercises() {
 		return this.exercises.stream()
 				.map(IExercise.class::cast)
 				.collect(Collectors.toList());
-	}
+    }
 
-	@Override
-	public String getTitle() {
-		return this.title;
-	}
+    @Override
+    public String getTitle() {
+        return this.title;
+    }
 
-	public void init(ArtemisRESTClient artemisRESTClient) throws AuthenticationException, JsonProcessingException {
-		this.exercises = this.exercises.stream()
-				.filter(exercise -> exercise.getShortName() != null) //happens sometimes...
-				.collect(Collectors.toList());
+    public void init(ArtemisRESTClient artemisRESTClient) throws AuthenticationException, JsonProcessingException {
+        if (exercises == null) {
+            exercises = List.of();
+        }
+        this.exercises = this.exercises.stream()
+                .filter(exercise -> exercise.getShortName() != null) // happens sometimes...
+                .collect(Collectors.toList());
 		for (ArtemisExercise artemisExercise : this.exercises) artemisExercise.init(artemisRESTClient);
-	}
+    }
 
-	@Override
-	public boolean isMandatory() {
-		return this.isMandatory;
-	}
+    @Override
+    public boolean isMandatory() {
+        return this.isMandatory;
+    }
 
-	@Override
-	public String toString() {
+    @Override
+    public String toString() {
 		return "ArtemisExerciseGroup [exerciseGroupId=" + this.exerciseGroupId + ", exercises=" + this.exercises + ", title="
 				+ this.title + ", isMandatory=" + this.isMandatory + "]";
-	}
+    }
 }
