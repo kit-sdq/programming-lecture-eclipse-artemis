a41d06d380a46817ff854fd9baee04b6f259defa
Robin Schulz <uyduk@student.kit.edu>
2021-09-21 17:14:19 +0200

    Dummy -> DTO (#93)

--- bundles/edu.kit.kastel.sdq.eclipse.grading.client/src/edu/kit/kastel/sdq/eclipse/grading/client/mappings/lock/ExerciseDTO.java
+++ bundles/edu.kit.kastel.sdq.eclipse.grading.client/src/edu/kit/kastel/sdq/eclipse/grading/client/mappings/lock/ExerciseDTO.java
@@ -4,14 +4,14 @@ import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 /**
- * This is only used in the json deserialization process. Much like {@link ParticipationDummy}
+ * This is only used in the json deserialization process. Much like {@link ParticipationDTO}
  */
-public class ExerciseDummy {
+public class ExerciseDTO {
 
 	private double maxPoints;
 
 	@JsonCreator
-	public ExerciseDummy(@JsonProperty("maxPoints") double maxPoints) {
+	public ExerciseDTO(@JsonProperty("maxPoints") double maxPoints) {
 		this.maxPoints = maxPoints;
 	}
 
