81f59242837a9054c8329c1e6df13fc2eca9ae39
Dominik Fuchß <dominik.fuchss@kit.edu>
2021-10-05 16:28:22 +0200

    Only show second round in Backlog iff activated

--- bundles/edu.kit.kastel.sdq.eclipse.grading.api/src/edu/kit/kastel/sdq/eclipse/grading/api/client/IArtemisClient.java
+++ bundles/edu.kit.kastel.sdq.eclipse.grading.api/src/edu/kit/kastel/sdq/eclipse/grading/api/client/IArtemisClient.java
@@ -16,82 +16,78 @@ import edu.kit.kastel.sdq.eclipse.grading.api.artemis.mapping.IParticipation;
 import edu.kit.kastel.sdq.eclipse.grading.api.artemis.mapping.ISubmission;
 
 public interface IArtemisClient {
-	/**
-	 * Clones exercise and a submission into one project.
-	 */
-	void downloadExerciseAndSubmission(IExercise exercise, ISubmission submission, File dir, IProjectFileNamingStrategy namingStrategy)
-			throws ArtemisClientException;
+    /**
+     * Clones exercise and a submission into one project.
+     */
+    void downloadExerciseAndSubmission(IExercise exercise, ISubmission submission, File dir, IProjectFileNamingStrategy namingStrategy)
+            throws ArtemisClientException;
 
-	/**
-	 *
-	 * @return the artemis "assessor" object (needed for submitting the assessment).
-	 * @throws ArtemisClientException if some errors occur while parsing the result.
-	 */
-	IAssessor getAssessor() throws ArtemisClientException;
+    /**
+     *
+     * @return the artemis "assessor" object (needed for submitting the assessment).
+     * @throws ArtemisClientException if some errors occur while parsing the result.
+     */
+    IAssessor getAssessor() throws ArtemisClientException;
 
-	/**
-	 *
-	 * @return all available courses, containing exercises and available submissions
-	 * @throws ArtemisClientException if some errors occur while parsing the result.
-	 */
-	List<ICourse> getCourses() throws ArtemisClientException;
+    /**
+     *
+     * @return all available courses, containing exercises and available submissions
+     * @throws ArtemisClientException if some errors occur while parsing the result.
+     */
+    List<ICourse> getCourses() throws ArtemisClientException;
 
-	default List<ISubmission> getSubmissions(IExercise exercise) throws ArtemisClientException {
-		List<ISubmission> submissions = new ArrayList<>();
-		submissions.addAll(this.getSubmissions(exercise, 0));
-		submissions.addAll(this.getSubmissions(exercise, 1));
-		return submissions;
-	}
+    default List<ISubmission> getSubmissions(IExercise exercise) throws ArtemisClientException {
+        List<ISubmission> submissions = new ArrayList<>(this.getSubmissions(exercise, 0));
+        // TODO This is just a fix that you can see second correction rounds iff available
+        if (exercise.getSecondCorrectionEnabled()) {
+            submissions.addAll(this.getSubmissions(exercise, 1));
+        }
+        return submissions;
+    }
 
-	/**
-	 *
-	 * @param exerciseID
-	 * @param assessedByTutor only return those submissions on which the caller has
-	 *                        (started, saved or submitted) the assessment.
-	 * @return submissions for the given exerciseID, filterable.
-	 * @throws ArtemisClientException if some errors occur while parsing the result.
-	 */
-	List<ISubmission> getSubmissions(IExercise exercise, int correctionRound) throws ArtemisClientException;
+    /**
+     *
+     * @param exerciseID
+     * @param assessedByTutor only return those submissions on which the caller has (started, saved or submitted) the
+     *                        assessment.
+     * @return submissions for the given exerciseID, filterable.
+     * @throws ArtemisClientException if some errors occur while parsing the result.
+     */
+    List<ISubmission> getSubmissions(IExercise exercise, int correctionRound) throws ArtemisClientException;
 
-	/**
-	 * Submit the assessment to Artemis. Must have been started by
-	 * {@link #startAssessment(int)} or {@link #startNextAssessment(int, int)}
-	 * before!
-	 *
-	 * @param participation THOU SHALT NOT PROVIDE THE SUBMISSIONID, HERE! The
-	 *                      participationID can be gotten from the
-	 *                      {@link ILockResult}, via {@link #startAssessment(int)}
-	 *                      or {@link #startNextAssessment(int, int)}! * @param
-	 *                      submit determine whether the assessment should be
-	 *                      submitted or just saved.
-	 * @param assessment    the assessment
-	 *
-	 * @throws ArtemisClientException
-	 */
-	void saveAssessment(IParticipation participation, boolean submit, AssessmentResult assessment) throws ArtemisClientException;
+    /**
+     * Submit the assessment to Artemis. Must have been started by {@link #startAssessment(int)} or
+     * {@link #startNextAssessment(int, int)} before!
+     *
+     * @param participation THOU SHALT NOT PROVIDE THE SUBMISSIONID, HERE! The participationID can be gotten from the
+     *                      {@link ILockResult}, via {@link #startAssessment(int)} or
+     *                      {@link #startNextAssessment(int, int)}! * @param submit determine whether the assessment
+     *                      should be submitted or just saved.
+     * @param assessment    the assessment
+     *
+     * @throws ArtemisClientException
+     */
+    void saveAssessment(IParticipation participation, boolean submit, AssessmentResult assessment) throws ArtemisClientException;
 
-	/**
-	 * Starts an assessment for the given submission. Acquires a lock in the
-	 * process.
-	 *
-	 * @param submission
-	 * @return the data gotten back, which is needed for submitting the assessment
-	 *         result correctly ({@link #saveAssessment(int, boolean, String)}
-	 * @throws ArtemisClientException if some errors occur while parsing the result.
-	 */
+    /**
+     * Starts an assessment for the given submission. Acquires a lock in the process.
+     *
+     * @param submission
+     * @return the data gotten back, which is needed for submitting the assessment result correctly
+     *         ({@link #saveAssessment(int, boolean, String)}
+     * @throws ArtemisClientException if some errors occur while parsing the result.
+     */
 
-	ILockResult startAssessment(ISubmission submission) throws ArtemisClientException;
+    ILockResult startAssessment(ISubmission submission) throws ArtemisClientException;
 
-	/**
-	 * Starts an assessment for any available submission (determined by artemis).
-	 * Acquires a lock in the process.
-	 *
-	 * @param correctionRound relevant for exams! may be 0 or 1
-	 * @return
-	 *         <li>the data gotten back. Needed for submitting correctly.
-	 *         <li><b>null</b> if there is no submission left to correct
-	 * @throws ArtemisClientException if some errors occur while parsing the result
-	 *                                or if authentication fails.
-	 */
-	Optional<ILockResult> startNextAssessment(IExercise exerciseID, int correctionRound) throws ArtemisClientException;
+    /**
+     * Starts an assessment for any available submission (determined by artemis). Acquires a lock in the process.
+     *
+     * @param correctionRound relevant for exams! may be 0 or 1
+     * @return
+     *         <li>the data gotten back. Needed for submitting correctly.
+     *         <li><b>null</b> if there is no submission left to correct
+     * @throws ArtemisClientException if some errors occur while parsing the result or if authentication fails.
+     */
+    Optional<ILockResult> startNextAssessment(IExercise exerciseID, int correctionRound) throws ArtemisClientException;
 }
