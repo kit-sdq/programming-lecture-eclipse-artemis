32b5ab8c3d9aecfcf7c592147f8fbeb1a65c73b7
Dominik Fuchß <dominik.fuchss@kit.edu>
2022-01-11 13:23:40 +0100

    Ordering for lists in artemis course.

--- bundles/edu.kit.kastel.sdq.eclipse.grading.client/src/edu/kit/kastel/sdq/eclipse/grading/client/mappings/ArtemisCourse.java
+++ bundles/edu.kit.kastel.sdq.eclipse.grading.client/src/edu/kit/kastel/sdq/eclipse/grading/client/mappings/ArtemisCourse.java
@@ -44,6 +44,7 @@ public class ArtemisCourse implements ICourse, Serializable {
 	public List<IExam> getExams() throws ArtemisClientException {
 		if (this.exams == null) {
 			this.exams = this.client.getExamsForCourse(this);
+			this.exams.sort((e1, e2) -> e1.getTitle().compareTo(e2.getTitle()));
 		}
 		return this.exams;
 	}
@@ -52,6 +53,7 @@ public class ArtemisCourse implements ICourse, Serializable {
 	public List<IExercise> getExercises() throws ArtemisClientException {
 		if (this.exercises == null) {
 			this.exercises = this.client.getNormalExercisesForCourse(this);
+			this.exercises.sort((e1, e2) -> e1.getShortName().compareTo(e2.getShortName()));
 		}
 		return this.exercises;
 	}
