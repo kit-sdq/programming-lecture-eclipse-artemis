kit-sdq/programming-lecture-eclipse-artemis

commit 9a69b042e46ef7cd7849ac6c1da807eb69de4c68

Remove deprecated Result String (#233)
* Remove deprecated result string

* Formatter

bundles/edu.kit.kastel.sdq.eclipse.common.api/src/edu/kit/kastel/eclipse/common/api/artemis/AssessmentResult.java
@@ -15,7 +15,6 @@ public class AssessmentResult implements Serializable {
 	private static final long serialVersionUID = -1703764424474018461L;
 
 	private int id;
-	private String resultString;
 	private String assessmentType;
 	private double score;
 	private boolean rated;
@@ -24,10 +23,9 @@ public class AssessmentResult implements Serializable {
 	private User assessor;
 	private List<Feedback> feedbacks;
 
-	public AssessmentResult(int id, String resultString, String assessmentType, double score, boolean rated, boolean hasFeedback, String completionDate,
-			User assessor, List<Feedback> feedbacks) {
+	public AssessmentResult(int id, String assessmentType, double score, boolean rated, boolean hasFeedback, String completionDate, User assessor,
+			List<Feedback> feedbacks) {
 		this.id = id;
-		this.resultString = resultString;
 		this.assessmentType = assessmentType;
 		this.score = score;
 		this.rated = rated;
@@ -55,10 +53,6 @@ public class AssessmentResult implements Serializable {
 
 	public int getId() {
 		return this.id;
-	}
-
-	public String getResultString() {
-		return this.resultString;
 	}
 
 	public double getScore() {

bundles/edu.kit.kastel.sdq.eclipse.common.core/src/edu/kit/kastel/eclipse/common/core/artemis/AnnotationMapper.java
@@ -120,22 +120,6 @@ public class AnnotationMapper {
 		return absoluteScore / this.exercise.getMaxPoints() * 100D;
 	}
 
-	private String calculateResultString(final List<Feedback> allFeedbacks, final double absoluteScore) {
-		final List<Feedback> autoFeedbacks = //
-				allFeedbacks.stream().filter(Objects::nonNull).filter(feedback -> feedback.getFeedbackType() == FeedbackType.AUTOMATIC)
-						.collect(Collectors.toList());
-
-		final List<Feedback> tests = autoFeedbacks.stream().filter(f -> f.getReference() == null).collect(Collectors.toList());
-		long positiveTests = tests.stream().filter(feedback -> feedback.getPositive() != null && feedback.getPositive()).count();
-		long numberOfTests = tests.size();
-
-		// ENHANCE We may add "Issues" as text here iff activated ?
-		String result = "";
-		result += String.format("%d of %d passed, ", positiveTests, numberOfTests);
-		result += String.format("%s of %s points", nf.format(absoluteScore), nf.format(this.exercise.getMaxPoints()));
-		return result;
-	}
-
 	private String convertAnnotationsToJSONString(final List<IAnnotation> givenAnnotations) throws JsonProcessingException {
 		return oom.writeValueAsString(givenAnnotations);
 	}
@@ -171,8 +155,7 @@ public class AnnotationMapper {
 		final double absoluteScore = Math.min(Math.max(0.D, this.calculateAbsoluteScore(allFeedbacks)), this.exercise.getMaxPoints());
 		final double relativeScore = this.calculateRelativeScore(absoluteScore);
 
-		return new AssessmentResult(this.submission.getSubmissionId(), this.calculateResultString(allFeedbacks, absoluteScore), "SEMI_AUTOMATIC", relativeScore,
-				true, true, null, this.assessor, allFeedbacks);
+		return new AssessmentResult(this.submission.getSubmissionId(), "SEMI_AUTOMATIC", relativeScore, true, true, null, this.assessor, allFeedbacks);
 	}
 
 	/**

bundles/edu.kit.kastel.sdq.eclipse.common.api/src/edu/kit/kastel/eclipse/common/api/artemis/mapping/ResultsDTO.java
@@ -27,8 +27,6 @@ public class ResultsDTO implements Serializable {
 	public double score;
 	@JsonProperty
 	public Boolean rated;
-	@JsonProperty
-	public String resultString;
 
 	public ResultsDTO() {
 		// NOP

bundles/edu.kit.kastel.sdq.eclipse.common.view/src/edu/kit/kastel/eclipse/common/view/ui/AbstractResultTab.java
@@ -1,6 +1,8 @@
 /* Licensed under EPL-2.0 2022. */
 package edu.kit.kastel.eclipse.common.view.ui;
 
+import static edu.kit.kastel.eclipse.common.view.languages.LanguageSettings.I18N;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -16,11 +18,9 @@ import edu.kit.kastel.eclipse.common.api.artemis.mapping.Feedback;
 import edu.kit.kastel.eclipse.common.api.artemis.mapping.FeedbackType;
 import edu.kit.kastel.eclipse.common.api.artemis.mapping.IExercise;
 import edu.kit.kastel.eclipse.common.api.model.IMistakeType;
-import edu.kit.kastel.eclipse.common.api.util.Triple;
+import edu.kit.kastel.eclipse.common.api.util.Pair;
 import edu.kit.kastel.eclipse.common.core.model.annotation.Annotation;
 import edu.kit.kastel.eclipse.common.view.utilities.AssessmentUtilities;
-
-import static edu.kit.kastel.eclipse.common.view.languages.LanguageSettings.I18N;
 
 public abstract class AbstractResultTab extends AbstractResultTabCompositeController {
 
@@ -42,9 +42,9 @@ public abstract class AbstractResultTab extends AbstractResultTabCompositeContro
 	 * Get the latest result of the currently selected submission.
 	 *
 	 * @return the current feedback as
-	 *         {@code [String completionTime, String resultString, List<Feedback> feedbacks]}
+	 *         {@code [String completionTime, List<Feedback> feedbacks]}
 	 */
-	protected abstract Triple<String, String, List<Feedback>> getCurrentResultAndFeedback();
+	protected abstract Pair<String, List<Feedback>> getCurrentResultAndFeedback();
 
 	/**
 	 * Get the project name of the currently selected project in eclipse to add
@@ -69,11 +69,10 @@ public abstract class AbstractResultTab extends AbstractResultTabCompositeContro
 		var currentSourceDirectory = this.getCurrentSourceDirectoryRelative();
 
 		if (!resultFeedback.isEmpty()) {
-			// IExercise currentExercise, String completionTime, String resultString,
-			// List<Feedback> feedbacks
-			this.handleNewResult(currentExercise, resultFeedback.first(), resultFeedback.second(), resultFeedback.third());
+			// IExercise currentExercise, String completionTime, List<Feedback> feedbacks
+			this.handleNewResult(currentExercise, resultFeedback.first(), resultFeedback.second());
 			if (currentProjectFileForAnnotation != null) {
-				this.createAnnotationsMarkers(currentProjectFileForAnnotation, currentSourceDirectory, resultFeedback.third());
+				this.createAnnotationsMarkers(currentProjectFileForAnnotation, currentSourceDirectory, resultFeedback.second());
 			}
 		} else {
 			this.testTable.removeAll();
@@ -81,12 +80,12 @@ public abstract class AbstractResultTab extends AbstractResultTabCompositeContro
 		}
 	}
 
-	private void addResultToTab(IExercise currentExercise, String completionTime, String resultString, List<Feedback> feedbacks) {
+	private void addResultToTab(IExercise currentExercise, String completionTime, List<Feedback> feedbacks) {
 		boolean successOfAutomaticTests = this.calculateSuccessOfAutomaticTests(feedbacks);
 		double points = this.calculatePoints(currentExercise, feedbacks);
 		double score = this.calculateScore(currentExercise, points);
 
-		this.setSuccessAndScore(currentExercise, successOfAutomaticTests, points, score, completionTime, resultString);
+		this.setSuccessAndScore(currentExercise, successOfAutomaticTests, points, score, completionTime);
 		this.layout();
 	}
 
@@ -127,10 +126,10 @@ public abstract class AbstractResultTab extends AbstractResultTabCompositeContro
 
 	}
 
-	private void handleNewResult(IExercise currentExercise, String completionTime, String resultString, List<Feedback> feedbacks) {
+	private void handleNewResult(IExercise currentExercise, String completionTime, List<Feedback> feedbacks) {
 		this.testTable.removeAll();
 		this.addFeedbackToTable(this.testTable, feedbacks);
-		this.addResultToTab(currentExercise, completionTime, resultString, feedbacks);
+		this.addResultToTab(currentExercise, completionTime, feedbacks);
 	}
 
 	/**

bundles/edu.kit.kastel.sdq.eclipse.common.view/src/edu/kit/kastel/eclipse/common/view/ui/AbstractResultTabCompositeController.java
@@ -32,8 +32,7 @@ abstract class AbstractResultTabCompositeController extends AbstractResultTabCom
 
 	protected abstract void reloadFeedbackForExcerise();
 
-	protected void setSuccessAndScore(IExercise currentExercise, boolean successOfAutomaticTests, double points, double score, String completionTime,
-			String resultString) {
+	protected void setSuccessAndScore(IExercise currentExercise, boolean successOfAutomaticTests, double points, double score, String completionTime) {
 		var display = getDisplay();
 		String title = currentExercise == null ? I18N().unknownTask() : currentExercise.getTitle();
 
@@ -42,7 +41,7 @@ abstract class AbstractResultTabCompositeController extends AbstractResultTabCom
 
 		this.lblTaskname.setText(title);
 		this.lblTaskdetails.setText(completionTime == null ? "" : completionTime);
-		this.lblScore.setText(resultString == null ? String.format(Locale.ENGLISH, I18N().score() + ": %.2f%%", score) : resultString);
+		this.lblScore.setText(String.format(Locale.ENGLISH, I18N().score() + ": %.2f%%", score));
 		this.lblPoints.setText(String.format(Locale.ENGLISH, I18N().points() + ": %.2f", points));
 	}
 

bundles/edu.kit.kastel.sdq.eclipse.grading.view/src/edu/kit/kastel/eclipse/grading/view/assessment/ResultTab.java
@@ -9,7 +9,7 @@ import edu.kit.kastel.eclipse.common.api.artemis.mapping.Feedback;
 import edu.kit.kastel.eclipse.common.api.artemis.mapping.FeedbackType;
 import edu.kit.kastel.eclipse.common.api.artemis.mapping.IExercise;
 import edu.kit.kastel.eclipse.common.api.controller.IGradingSystemwideController;
-import edu.kit.kastel.eclipse.common.api.util.Triple;
+import edu.kit.kastel.eclipse.common.api.util.Pair;
 import edu.kit.kastel.eclipse.common.view.ui.AbstractResultTab;
 
 public class ResultTab extends AbstractResultTab {
@@ -31,14 +31,13 @@ public class ResultTab extends AbstractResultTab {
 	}
 
 	@Override
-	protected Triple<String, String, List<Feedback>> getCurrentResultAndFeedback() {
+	protected Pair<String, List<Feedback>> getCurrentResultAndFeedback() {
 		var submission = this.controller.getCurrentAssessmentController().getSubmission();
 		var feedbacks = this.controller.getArtemisController().getAllFeedbacksGottenFromLocking(submission);
 		// TODO Extract these strings from Artemis request
 		String completionTime = null;
-		String resultText = null;
 
-		return new Triple<>(completionTime, resultText, feedbacks.stream().filter(f -> f.getFeedbackType() == FeedbackType.AUTOMATIC).toList());
+		return new Pair<>(completionTime, feedbacks.stream().filter(f -> f.getFeedbackType() == FeedbackType.AUTOMATIC).toList());
 	}
 
 	@Override

bundles/edu.kit.kastel.sdq.eclipse.student.view/src/edu/kit/kastel/eclipse/student/view/ui/ResultTab.java
@@ -9,7 +9,7 @@ import org.eclipse.swt.widgets.TabFolder;
 import edu.kit.kastel.eclipse.common.api.artemis.mapping.Feedback;
 import edu.kit.kastel.eclipse.common.api.artemis.mapping.IExercise;
 import edu.kit.kastel.eclipse.common.api.client.websocket.WebsocketCallback;
-import edu.kit.kastel.eclipse.common.api.util.Triple;
+import edu.kit.kastel.eclipse.common.api.util.Pair;
 import edu.kit.kastel.eclipse.common.view.ui.AbstractResultTab;
 import edu.kit.kastel.eclipse.student.view.controllers.StudentViewController;
 
@@ -26,12 +26,12 @@ public class ResultTab extends AbstractResultTab implements WebsocketCallback {
 	}
 
 	@Override
-	protected Triple<String, String, List<Feedback>> getCurrentResultAndFeedback() {
+	protected Pair<String, List<Feedback>> getCurrentResultAndFeedback() {
 		var details = this.viewController.getFeedbackExcerise();
 		if (details.isEmpty()) {
-			return new Triple<>(null, null, null);
+			return new Pair<>(null, null);
 		}
-		return new Triple<>(details.first().completionDateAsString(), details.first().resultString, details.second());
+		return new Pair<>(details.first().completionDateAsString(), details.second());
 	}
 
 	@Override

commit 6d00ab686f1258216899750cbff1c1b3aa2020b7

Fixed some issue introduced in #233

bundles/edu.kit.kastel.sdq.eclipse.common.api/src/edu/kit/kastel/eclipse/common/api/artemis/AssessmentResult.java
@@ -4,66 +4,46 @@ package edu.kit.kastel.eclipse.common.api.artemis;
 import java.io.Serializable;
 import java.util.List;
 
-import com.fasterxml.jackson.annotation.JsonInclude;
-import com.fasterxml.jackson.annotation.JsonInclude.Include;
+import com.fasterxml.jackson.annotation.JsonProperty;
 
 import edu.kit.kastel.eclipse.common.api.artemis.mapping.Feedback;
 import edu.kit.kastel.eclipse.common.api.artemis.mapping.User;
 
-@JsonInclude(Include.NON_NULL)
 public class AssessmentResult implements Serializable {
 	private static final long serialVersionUID = -1703764424474018461L;
 
-	private int id;
+	@JsonProperty
+	public final int id;
+	@JsonProperty
 	private String assessmentType;
+	@JsonProperty
 	private double score;
+	@JsonProperty
 	private boolean rated;
+	@JsonProperty
 	private boolean hasFeedback;
-	private String completionDate;
+	@JsonProperty
 	private User assessor;
+	@JsonProperty
 	private List<Feedback> feedbacks;
+	@JsonProperty
+	private int codeIssueCount;
+	@JsonProperty
+	private int passedTestCaseCount;
+	@JsonProperty
+	private int testCaseCount;
 
-	public AssessmentResult(int id, String assessmentType, double score, boolean rated, boolean hasFeedback, String completionDate, User assessor,
-			List<Feedback> feedbacks) {
+	public AssessmentResult(int id, String assessmentType, double score, boolean rated, boolean hasFeedback, User assessor, List<Feedback> feedbacks,
+			int codeIssueCount, int passedTestCaseCount, int testCaseCount) {
 		this.id = id;
 		this.assessmentType = assessmentType;
 		this.score = score;
 		this.rated = rated;
 		this.hasFeedback = hasFeedback;
-		this.completionDate = completionDate;
 		this.assessor = assessor;
 		this.feedbacks = feedbacks;
-	}
-
-	public String getAssessmentType() {
-		return this.assessmentType;
-	}
-
-	public User getAssessor() {
-		return this.assessor;
-	}
-
-	public String getCompletionDate() {
-		return this.completionDate;
-	}
-
-	public List<Feedback> getFeedbacks() {
-		return this.feedbacks;
-	}
-
-	public int getId() {
-		return this.id;
-	}
-
-	public double getScore() {
-		return this.score;
-	}
-
-	public boolean isHasFeedback() {
-		return this.hasFeedback;
-	}
-
-	public boolean isRated() {
-		return this.rated;
+		this.codeIssueCount = codeIssueCount;
+		this.passedTestCaseCount = passedTestCaseCount;
+		this.testCaseCount = testCaseCount;
 	}
 }

bundles/edu.kit.kastel.sdq.eclipse.common.api/src/edu/kit/kastel/eclipse/common/api/artemis/mapping/Feedback.java
@@ -4,6 +4,7 @@ package edu.kit.kastel.eclipse.common.api.artemis.mapping;
 import java.io.Serializable;
 import java.util.Objects;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonInclude.Include;
 import com.fasterxml.jackson.annotation.JsonProperty;
@@ -183,5 +184,10 @@ public class Feedback implements Comparable<Feedback>, Serializable {
 		return Objects.equals(this.credits, other.credits) && Objects.equals(this.detailText, other.detailText) && Objects.equals(this.id, other.id)
 				&& Objects.equals(this.positive, other.positive) && Objects.equals(this.reference, other.reference) && Objects.equals(this.text, other.text)
 				&& Objects.equals(this.type, other.type) && Objects.equals(this.visibility, other.visibility);
+	}
+
+	@JsonIgnore
+	public boolean isSCA() {
+		return this.text != null && this.text.startsWith("SCAFeedbackIdentifier");
 	}
 }

bundles/edu.kit.kastel.sdq.eclipse.common.client/src/edu/kit/kastel/eclipse/common/client/rest/AssessmentArtemisClient.java
@@ -41,7 +41,7 @@ public class AssessmentArtemisClient extends AbstractArtemisClient implements IA
 	@Override
 	public void saveAssessment(int participationId, boolean submit, AssessmentResult assessment) throws ArtemisClientException {
 		String assessmentPayload = this.payload(assessment);
-		log.info(String.format("Saving assessment for submission %s with json: %s", assessment.getId(), assessmentPayload));
+		log.info(String.format("Saving assessment for submission %s with json: %s", assessment.id, assessmentPayload));
 
 		final Response rsp = this.endpoint.path(PARTICIPATION_PATHPART).path(String.valueOf(participationId)) //
 				.path(MANUAL_RESULT_PATHPART) //
@@ -54,7 +54,8 @@ public class AssessmentArtemisClient extends AbstractArtemisClient implements IA
 	@Override
 	public ILockResult startAssessment(ISubmission submission) throws ArtemisClientException {
 		final Response rsp = this.endpoint.path(PROGRAMMING_SUBMISSION_PATHPART).path(String.valueOf(submission.getSubmissionId())).path(LOCK_QUERY_PARAM)
-				.request().header(AUTHORIZATION_NAME, this.token).buildGet().invoke(); // synchronous variant
+				.request().header(AUTHORIZATION_NAME, this.token).buildGet().invoke(); // synchronous
+																						// variant
 		this.throwIfStatusUnsuccessful(rsp);
 		return this.read(rsp.readEntity(String.class), LockResult.class);
 	}

bundles/edu.kit.kastel.sdq.eclipse.common.core/src/edu/kit/kastel/eclipse/common/core/artemis/AnnotationMapper.java
@@ -155,7 +155,17 @@ public class AnnotationMapper {
 		final double absoluteScore = Math.min(Math.max(0.D, this.calculateAbsoluteScore(allFeedbacks)), this.exercise.getMaxPoints());
 		final double relativeScore = this.calculateRelativeScore(absoluteScore);
 
-		return new AssessmentResult(this.submission.getSubmissionId(), "SEMI_AUTOMATIC", relativeScore, true, true, null, this.assessor, allFeedbacks);
+		final List<Feedback> initialFeedback = getFilteredPreexistentFeedbacks(FeedbackType.AUTOMATIC);
+		final List<Feedback> tests = initialFeedback.stream().filter(f -> f.getReference() == null).collect(Collectors.toList());
+
+		int codeIssueCount = (int) initialFeedback.stream().filter(Feedback::isSCA).count();
+		int passedTestCaseCount = (int) tests.stream() //
+				.filter(feedback -> feedback.getPositive() != null && feedback.getPositive()).count();
+
+		return new AssessmentResult(this.submission.getSubmissionId(), "SEMI_AUTOMATIC", //
+				relativeScore, true, true, this.assessor, allFeedbacks, //
+				codeIssueCount, passedTestCaseCount, tests.size() //
+		);
 	}
 
 	/**