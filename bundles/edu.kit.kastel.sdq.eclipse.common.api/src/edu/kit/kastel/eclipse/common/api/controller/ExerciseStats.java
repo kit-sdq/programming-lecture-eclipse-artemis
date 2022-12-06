753c9905c222886a46b93a41cc611091d4278ddd
Dominik Fuchß <develop@fuchss.org>
2022-12-06 23:14:01 +0100

    Reimplement statistics

---
+++ bundles/edu.kit.kastel.sdq.eclipse.common.api/src/edu/kit/kastel/eclipse/common/api/controller/ExerciseStats.java
@@ -0,0 +1,5 @@
+/* Licensed under EPL-2.0 2022. */
+package edu.kit.kastel.eclipse.common.api.controller;
+
+public record ExerciseStats(int totalAssessments, int totalSubmissions, int locked, int submittedByTutor) {
+}
