625f9ed0277f9cd11eef8569c8a375bc630ce677
Dominik Fuchß <dominik.fuchss@kit.edu>
2022-12-07 23:59:01 +0100

    Statistics for Assessment (#274)

---
+++ bundles/edu.kit.kastel.sdq.eclipse.common.client/src/edu/kit/kastel/eclipse/common/client/mappings/stats/Stats.java
@@ -0,0 +1,10 @@
+/* Licensed under EPL-2.0 2022. */
+package edu.kit.kastel.eclipse.common.client.mappings.stats;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public record Stats(@JsonProperty Timing numberOfSubmissions, //
+		@JsonProperty Timing[] numberOfAssessmentsOfCorrectionRounds, //
+		@JsonProperty int totalNumberOfAssessmentLocks //
+) {
+}
